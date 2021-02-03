31d3c5adb87db98ef57414514d947c3656ebf49c
PabloYepes27 <juanpabloyepest@gmail.com>
2021-02-03 10:51:50 -0500

    Fixing test suite for CI

--- tests/test_controllers.py
+++ tests/test_controllers.py
@@ -43,29 +43,29 @@ class ControllersTestCase(unittest.TestCase):
         res = self.app.post(URL, json=reservation)
         self.assertEqual(res.status_code, 200)
 
-    # def test_delete_space(self):
-    #     """test the GET method"""
-    #     today = datetime.today()
-    #     date_in = format_dates(today + timedelta(3))
-    #     date_out = format_dates(today + timedelta(5))
+    def test_delete_space(self):
+        """test the GET method"""
+        today = datetime.today()
+        date_in = format_dates(today + timedelta(3))
+        date_out = format_dates(today + timedelta(5))
 
-    #     reservation = {
-    #         "total_value": 20000,
-    #         "payed_value": 4200,
-    #         "status": 1,
-    #         "date_in": date_in,
-    #         "date_out": date_out
-    #     }
-    #     self.app.post(URL, json=reservation)
+        reservation = {
+            "total_value": 20000,
+            "payed_value": 4200,
+            "status": 1,
+            "date_in": date_in,
+            "date_out": date_out
+        }
+        self.app.post(URL, json=reservation)
 
-    #     res = self.app.get(URL)
-    #     response = json.loads(res.get_data().decode(sys.getdefaultencoding()))
+        res = self.app.get(URL)
+        response = json.loads(res.get_data().decode(sys.getdefaultencoding()))
 
-    #     reservation = {"space_id": 1}
-    #     res = self.app.delete(URL,
-    #                           data=reservation,
-    #                           headers={'Content-Type': 'application/JSON'})
-    #     self.assertEqual(res.status_code, 400)
+        reservation = {"space_id": 1}
+        res = self.app.delete(URL,
+                              data=reservation,
+                              headers={'Content-Type': 'application/JSON'})
+        self.assertEqual(res.status_code, 400)
 
     def tearDown(self):
         """ restart the tables"""

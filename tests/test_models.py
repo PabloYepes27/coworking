19cf56b66ef55d7895445bdb5d70ab442306e2e8
PabloYepes27 <juanpabloyepest@gmail.com>
2021-02-03 12:41:30 -0500

    cleaning file

--- tests/test_models.py
+++ tests/test_models.py
@@ -31,7 +31,29 @@ class BussinessLogicTestCase(unittest.TestCase):
     """Unittests for the domaing layer"""
 
 
-    def test_valid_date(self):
+    def test_valid_date_passed(self):
+        good_date_in = "12/05/2019"
+        bad_date_in = "2019/05/21"
+        good_date_out = "05/12/2018"
+        bad_date_out = "15/02/21"
+
+        message1, e1 = valid_date(good_date_in, good_date_out)
+        message2, e2 = valid_date(bad_date_in, bad_date_out)
+
+        self.assertEqual(e1, 200)
+
+    def test_valid_date_failed(self):
+        good_date_in = "12/05/2019"
+        bad_date_in = "2019/05/21"
+        good_date_out = "05/12/2018"
+        bad_date_out = "15/02/21"
+
+        message1, e1 = valid_date(good_date_in, good_date_out)
+        message2, e2 = valid_date(bad_date_in, bad_date_out)
+
+        self.assertEqual(e2, 400)
+
+    def test_valid_date_message(self):
         good_date_in = "12/05/2019"
         bad_date_in = "2019/05/21"
         good_date_out = "05/12/2018"
@@ -41,8 +63,6 @@ class BussinessLogicTestCase(unittest.TestCase):
         message1, e1 = valid_date(good_date_in, good_date_out)
         message2, e2 = valid_date(bad_date_in, bad_date_out)
 
-        self.assertEqual(e1, 200)
-        self.assertEqual(e2, 400)
         self.assertEqual(message2, expected_bad_message)
 
 
@@ -61,7 +81,30 @@ class BussinessLogicTestCase(unittest.TestCase):
         self.assertEqual(message2, expected_bad_message)
 
 
-    def test_valid_reservation_duration(self):
+    def test_valid_reservation_duration_passed(self):
+        today = datetime.today()
+        good_date_in = format_dates(today + timedelta(4))
+        good_date_out = format_dates(today + timedelta(6))
+        bad_date_out = format_dates(today + timedelta(4) + timedelta(15))
+  
+
+        message1, e1 = valid_reservation_duration(good_date_in, good_date_out)
+        message2, e2 = valid_reservation_duration(good_date_in, bad_date_out)
+
+        self.assertEqual(e1, 200)
+
+    def test_valid_reservation_duration_failed(self):
+        today = datetime.today()
+        good_date_in = format_dates(today + timedelta(4))
+        good_date_out = format_dates(today + timedelta(6))
+        bad_date_out = format_dates(today + timedelta(4) + timedelta(15))
+
+        message1, e1 = valid_reservation_duration(good_date_in, good_date_out)
+        message2, e2 = valid_reservation_duration(good_date_in, bad_date_out)
+
+        self.assertEqual(e2, 400)
+
+    def test_valid_reservation_duration_message(self):
         today = datetime.today()
         good_date_in = format_dates(today + timedelta(4))
         good_date_out = format_dates(today + timedelta(6))
@@ -72,12 +115,30 @@ class BussinessLogicTestCase(unittest.TestCase):
         message1, e1 = valid_reservation_duration(good_date_in, good_date_out)
         message2, e2 = valid_reservation_duration(good_date_in, bad_date_out)
 
-        self.assertEqual(e1, 200)
-        self.assertEqual(e2, 400)
         self.assertEqual(message2, expected_bad_message)
 
 
-    def test_check_payment(self):
+    def test_check_payment_passed(self):
+        total_value = 50000
+        good_payed_value = 7000
+        bad_payed_value = 2000
+
+        message1, e1 = check_payment(total_value, good_payed_value)
+        message2, e2 = check_payment(total_value, bad_payed_value)
+
+        self.assertEqual(e1, 200)
+
+    def test_check_payment_failed(self):
+        total_value = 50000
+        good_payed_value = 7000
+        bad_payed_value = 2000  
+
+        message1, e1 = check_payment(total_value, good_payed_value)
+        message2, e2 = check_payment(total_value, bad_payed_value)
+
+        self.assertEqual(e2, 400)
+
+    def test_check_payment_failed_message(self):
         total_value = 50000
         good_payed_value = 7000
         bad_payed_value = 2000
@@ -87,12 +148,11 @@ class BussinessLogicTestCase(unittest.TestCase):
         message1, e1 = check_payment(total_value, good_payed_value)
         message2, e2 = check_payment(total_value, bad_payed_value)
 
-        self.assertEqual(e1, 200)
-        self.assertEqual(e2, 400)
         self.assertEqual(message2, expected_bad_message)
 
-    # @mock.patch("validation_dates.py", return_value=[])
-    # def test_convert_to_db():
+    # @mock.patch("validation_date.valid_date", return_value=[])
+    # def test_convert_to_db(mock_response):
+        # mock_response.return_value = []
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
